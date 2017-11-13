35f4aef505a7ca55e4950c2b13cfdf93f463afa8
Andrew Forward <aforward@gmail.com>
2017-11-13 09:25:54 -0500

    Add :ok/:error example

--- test/myclient_test.exs
+++ test/myclient_test.exs
@@ -18,4 +18,15 @@ defmodule MyclientTest do
       headers: _headers} = HTTPoison.get!(url)
   end
 
+  test "make :ok/:error call" do
+    url = "https://raw.githubusercontent.com/work-samples/myclient/master/garbage"
+    {:ok, %HTTPoison.Response{body: _body,
+                              status_code: 404,
+                              request_url: ^url,
+                              headers: _headers}} = HTTPoison.get(url)
+
+    url = "ppq://url.com"
+    {:error, %HTTPoison.Error{reason: :nxdomain}} = HTTPoison.get(url)
+  end
+
 end
