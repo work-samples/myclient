7ba85f3e0d9a371c736d740ffe96a6870070a386
Andrew Forward <aforward@gmail.com>
2017-11-13 10:18:34 -0500

    Decode the JSON if possible

--- lib/myclient.ex
+++ lib/myclient.ex
@@ -7,6 +7,14 @@ defmodule Myclient do
         {:ok, %{body: raw_body, status_code: code}} -> {code, raw_body}
         {:error, %{reason: reason}} -> {:error, reason}
        end
+    |> (fn {ok, body} ->
+          body
+          |> Poison.decode(keys: :atoms)
+          |> case do
+               {:ok, parsed} -> {ok, parsed}
+               _ -> {:error, body}
+             end
+        end).()
   end
 
 end
