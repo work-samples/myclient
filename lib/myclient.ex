9f46175d81b47eced2fd54c4c16d410710439154
Andrew Forward <aforward@gmail.com>
2017-11-13 09:53:54 -0500

    Refactor to use pipe

--- lib/myclient.ex
+++ lib/myclient.ex
@@ -1,10 +1,12 @@
 defmodule Myclient do
 
   def get(url, headers \\ []) do
-    case HTTPoison.get(url, headers) do
-      {:ok, %{body: raw_body, status_code: code}} -> {code, raw_body}
-      {:error, %{reason: reason}} -> {:error, reason}
-    end
+    url
+    |> HTTPoison.get(headers)
+    |> case do
+        {:ok, %{body: raw_body, status_code: code}} -> {code, raw_body}
+        {:error, %{reason: reason}} -> {:error, reason}
+       end
   end
 
 end
